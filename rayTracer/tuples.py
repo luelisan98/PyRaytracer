7845618d146758015f19968bdcf822cd95da3488
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2023-09-17 11:40:12 -0600

    Operators
    Addition, subtraction, equals

--- rayTracer/tuples.py
+++ rayTracer/tuples.py
@@ -1,6 +1,9 @@
 class Tuples:
 	def __init__(self):
-		pass
+		self.x = 0
+		self.y = 0
+		self.z = 0
+		self.w = 1
 	
 	def Point(self,x,y,z):
 		self.x = x
@@ -25,3 +28,28 @@ class Tuples:
 			return True
 		else:
 			return False
+		
+	def __add__(self, Tuples):
+		tuple = type(Tuples)
+		tuple.x = self.x + Tuples.x
+		tuple.y = self.y + Tuples.y 
+		tuple.z = self.z + Tuples.z 
+		tuple.w = self.w + Tuples.w 
+		return tuple
+	
+	def __sub__(self, Tuples):
+		tuple = type(Tuples)
+		tuple.x = self.x - Tuples.x
+		tuple.y = self.y - Tuples.y 
+		tuple.z = self.z - Tuples.z 
+		tuple.w = self.w - Tuples.w 
+		return tuple
+	
+	def __eq__(self, Tuples):
+		if self.x == Tuples.x and \
+		    self.y == Tuples.y and \
+			  self.z == Tuples.z and \
+				self.w == Tuples.w:
+			return True
+		else:
+			return False 
