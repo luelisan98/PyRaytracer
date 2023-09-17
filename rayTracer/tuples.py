2f54ce6462b7be7c41242ce05e80ffa08438a916
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2023-09-17 12:49:50 -0600

    Refactoring + normalize tuples

--- rayTracer/tuples.py
+++ rayTracer/tuples.py
@@ -31,62 +31,39 @@ class Tuples:
 		else:
 			return False
 		
-	def __add__(self, Tuples):
-		tuple = type(Tuples)
-		tuple.x = self.x + Tuples.x
-		tuple.y = self.y + Tuples.y 
-		tuple.z = self.z + Tuples.z 
-		tuple.w = self.w + Tuples.w 
-		return tuple
-	
-	def __sub__(self, Tuples):
-		tuple = type(Tuples)
-		tuple.x = self.x - Tuples.x
-		tuple.y = self.y - Tuples.y 
-		tuple.z = self.z - Tuples.z 
-		tuple.w = self.w - Tuples.w 
-		return tuple
-	
+	def __add__(self, other):
+		return Tuples(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
+
+	def __sub__(self, other):
+		return Tuples(self.x - other.x, self.y - other.y, self.z - other.z, self.w - other.w)
+
 	def __neg__(self):
-		self.x = self.x * -1
-		self.y = self.y * -1
-		self.z = self.z * -1
-		self.w = self.w * -1
-		return self
+		return Tuples(-self.x, -self.y, -self.z, -self.w)
 	
 	def __eq__(self, Tuples):
-		if self.x == Tuples.x and \
+		return self.x == Tuples.x and \
 			self.y == Tuples.y and \
 			  self.z == Tuples.z and \
-				self.w == Tuples.w:
-			return True
-		else:
-			return False 
+				self.w == Tuples.w 
 		
 	def __mul__(self, num):
-		self.x = self.x * num
-		self.y = self.y * num
-		self.z = self.z * num
-		self.w = self.w * num
-		return self
+		return Tuples(self.x * num, self.y * num, self.z * num, self.w * num)
 
 	def __truediv__(self, num):
 		if num == 0:
 			num = 1 
-		self.x = self.x / num
-		self.y = self.y / num
-		self.z = self.z / num
-		self.w = self.w / num
-		return self
+		return Tuples(self.x / num, self.y / num, self.z / num, self.w / num)
 	
 	def magnitude(self, Tuple):
 		return sqrt(Tuple.x**2+Tuple.y**2+Tuple.z**2+Tuple.w**2) 
 
 	def equal(self, result, expected):
-		if result == expected:
-			return True
+		return result == expected
+	
+	def normalize(self, Tuple):
+		mag = self.magnitude(Tuple)
+		if mag == 0:
+			return Tuples(0, 0, 0, 0)
 		else:
-			return False
-	
-	
+			return Tuple / mag
 		
