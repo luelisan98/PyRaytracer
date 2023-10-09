8bb8e7513beab05fa4af261cfb52b3d726907e51
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2023-10-09 15:01:03 -0600

    Modify test cases

--- testing/test_lights.py
+++ testing/test_lights.py
@@ -1,14 +1,11 @@
 import pytest
-from rayTracer.lights import Lights
-from rayTracer.tuples import Tuples
-from rayTracer.colors import Colors
+from rayTracer.lights import PointLight
+from rayTracer.tuples import Tuple, Color
 
 def test_point_light_with_intensity_position():
-    intensity = Colors(1, 1, 1)
-    position = Tuples()
-    position = position.Point(0, 0, 0)
-    light = Lights()
-    light.point_light(position, intensity)
+    intensity = Color(1, 1, 1)
+    position = Tuple.point(0, 0, 0)
+    light = PointLight(position, intensity)
     assert light.position == position
     assert light.intensity == intensity
 
