aaf637d6c9887ef9a96a79763750bdfd7b4dacdd
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2023-10-10 12:04:05 -0600

    Add rotation in x y and z in matrix transformations

--- rayTracer/transformations.py
+++ rayTracer/transformations.py
@@ -18,14 +18,35 @@ class Transformations:
 		self.transformation.mat[2][2] = z
 		return self.transformation
 	
-	def rotation_x(self,angle_degrees):
-		angle_radians = math.radians(angle_degrees)
-		cos_r = math.cos(angle_radians)
-		sin_r = math.sin(angle_radians)
-
+	def rotation_x(self, angle):
+		
+		#angle_radians = math.radians(angle_degrees)
+		cos_r = math.cos(angle)
+		sin_r = math.sin(angle)
 		self.transformation.mat[1][1] = cos_r
 		self.transformation.mat[1][2] = -sin_r
 		self.transformation.mat[2][1] = sin_r
 		self.transformation.mat[2][2] = cos_r
-
+		return self.transformation
+	
+	def rotation_y(self, angle):
+		
+		#angle_radians = math.radians(angle_degrees)
+		cos_r = math.cos(angle)
+		sin_r = math.sin(angle)
+		self.transformation.mat[0][0] = cos_r
+		self.transformation.mat[0][2] = sin_r
+		self.transformation.mat[2][0] = -sin_r
+		self.transformation.mat[2][2] = cos_r
+		return self.transformation
+	
+	def rotation_z(self, angle):
+		
+		#angle_radians = math.radians(angle_degrees)
+		cos_r = math.cos(angle)
+		sin_r = math.sin(angle)
+		self.transformation.mat[0][0] = cos_r
+		self.transformation.mat[0][1] = -sin_r
+		self.transformation.mat[1][0] = sin_r
+		self.transformation.mat[1][1] = cos_r
 		return self.transformation
