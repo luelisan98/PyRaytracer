242ffc54da8dfefbde3a7db9cd554ccef9f4745f
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2023-11-24 19:27:26 -0600

    Habemus patterns

--- rayTracer/pattern.py
+++ rayTracer/pattern.py
@@ -7,10 +7,23 @@ class Pattern():
 	def __init__(self):
 		self.transform = Matrix(4,4).identity()
 
+	def test_pattern(self):
+		p = Pattern()
+		return p
+
 	def pattern_at_shape(self, obj, world_point):
 		object_point = obj.transform.inverse() * world_point
 		pattern_point = self.transform.inverse() * object_point
 		return self.pattern_at(pattern_point)
+	
+	def set_pattern_transform(self, transformation):
+		self.transform = transformation
+
+	def __eq__(self, other):
+			return (
+				self.transform == other.transform and
+				self.a == other.a and
+				self.b == other.b )
 
 
 class Stripe(Pattern):
@@ -24,11 +37,51 @@ class Stripe(Pattern):
 				return self.a 
 			else: 
 				return self.b
-		
-		def __eq__(self, other):
-			return (
-				self.transform == other.transform and
-				self.a == other.a and
-				self.b == other.b )
-
 	
+
+class Gradient(Pattern):
+	def __init__(self, color1, color2):
+		super().__init__()
+		self.a = color1
+		self.b = color2
+	
+	def pattern_at(self,point):
+		distance = self.b - self.a 
+		fraction = point.x - math.floor(point.x)
+		return self.a + distance * fraction
+
+class Ring(Pattern):
+	def __init__(self, color1, color2):
+		super().__init__()
+		self.a = color1
+		self.b = color2
+
+	def pattern_at(self, point):
+		in_ring = math.floor(math.sqrt((point.x ** 2) + (point.z ** 2))) % 2 == 0
+		if in_ring:
+			return self.a 
+		else:
+			return self.b
+		
+class Checker(Pattern):
+	def __init__(self, color1, color2, uv_map = False):
+		super().__init__()
+		self.a = color1
+		self.b = color2
+		self.uv_map = uv_map
+
+	def pattern_at(self,point):
+		if self.uv_map is True:
+			theta = math.atan2(point.x, point.z) + math.pi
+			r = point.magnitude()
+			phi = math.acos(point.y / r)
+			u = theta / (2 * math.pi)
+			v = phi / math.pi
+			if (math.floor(u * 20) + math.floor(v * 10)) % 2 == 0:
+				return self.a
+			return self.b
+
+		in_check = (math.floor(point.x) + math.floor(point.y) + math.floor(point.z)) % 2 == 0
+		if in_check is True:
+			return self.a
+		return self.b
--- testing/test_pattern.py
+++ testing/test_pattern.py
@@ -1,9 +1,9 @@
 from rayTracer.colors import Colors
-from rayTracer.pattern import Stripe
+from rayTracer.pattern import Stripe,Pattern,Gradient,Ring,Checker
 from rayTracer.tuples import Tuples
 from rayTracer.sphere import Sphere
 from rayTracer.transformations import Transformations
-
+from rayTracer.matrix import Matrix
 
 import pytest 
 
@@ -17,11 +17,19 @@ def black():
 	return Colors(0,0,0)
 
 def test_stripe_pattern():
-
 	pattern = Stripe(white, black)
-
 	assert pattern.a == white
 	assert pattern.b == black
+
+def test_default_pattern():
+	pattern = Pattern().test_pattern()
+	assert pattern.transform == Matrix(4,4).identity()
+
+def test_pattern_transform():
+	pattern = Pattern().test_pattern()
+	pattern.set_pattern_transform(Transformations().translation(1,2,3))
+	assert pattern.transform == Transformations().translation(1,2,3)
+
 
 def test_pattern_at_y():
 	pattern = Stripe(white, black)
@@ -46,7 +54,7 @@ def test_pattern_at_x():
 	
 def test_pattern_obj_transform():
 	obj = Sphere()
-	obj.transform = Transformations().scaling(2,2,2)
+	obj.set_transform(Transformations().scaling(2,2,2))
 	pattern = Stripe(white,black)
 	c = pattern.pattern_at_shape(obj, Tuples().Point(1.5,0,0))
 	assert c == white
@@ -54,14 +62,46 @@ def test_pattern_obj_transform():
 def test_pattern_pattern_transform():
 	obj = Sphere()
 	pattern = Stripe(white,black)
-	pattern.transform = Transformations().scaling(2,2,2)
+	pattern.set_pattern_transform(Transformations().scaling(2,2,2))
 	c = pattern.pattern_at_shape(obj, Tuples().Point(1.5,0,0))
 	assert c == white
 
 def test_pattern_obj_pattern_transform():
 	obj = Sphere()
-	obj.transform = Transformations().scaling(2,2,2)
+	obj.set_transform( Transformations().scaling(2,2,2))
 	pattern = Stripe(white,black)
-	pattern.transform = Transformations().translation(0.5,0,0)
+	pattern.set_pattern_transform(Transformations().translation(0.5,0,0))
 	c = pattern.pattern_at_shape(obj, Tuples().Point(2.5,0,0))
 	assert c == white
+
+def test_gradient():
+	pattern = Gradient(Colors(1,1,1), Colors(0,0,0))
+	assert pattern.pattern_at(Tuples().Point(0,0,0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0.25,0,0)) == Colors(0.75,0.75,0.75)
+	assert pattern.pattern_at(Tuples().Point(0.5,0,0)) == Colors(0.5,0.5,0.5)
+	assert pattern.pattern_at(Tuples().Point(0.75,0,0)) == Colors(0.25,0.25,0.25)
+
+def test_ring():
+	pattern = Ring(Colors(1,1,1), Colors(0,0,0))
+	assert pattern.pattern_at(Tuples().Point(0,0,0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(1,0,0)) == Colors(0,0,0)
+	assert pattern.pattern_at(Tuples().Point(0,0,1)) == Colors(0,0,0)
+	assert pattern.pattern_at(Tuples().Point(0.708,0,0.708)) == Colors(0,0,0)
+	
+def test_checker_repeat_x():
+	pattern = Checker(Colors(1,1,1), Colors(0,0,0))
+	assert pattern.pattern_at(Tuples().Point(0, 0, 0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0.99, 0, 0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(1.01, 0, 0)) == Colors(0,0,0)
+
+def test_checker_repeat_y():
+	pattern = Checker(Colors(1,1,1), Colors(0,0,0))
+	assert pattern.pattern_at(Tuples().Point(0, 0, 0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0, 0.99, 0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0, 1.01, 0)) == Colors(0,0,0)
+
+def test_checker_repeat_z():
+	pattern = Checker(Colors(1,1,1), Colors(0,0,0))
+	assert pattern.pattern_at(Tuples().Point(0, 0, 0)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0, 0, 0.99)) == Colors(1,1,1)
+	assert pattern.pattern_at(Tuples().Point(0, 0, 1.01)) == Colors(0,0,0)
